078e42cb8948e9441709ec98f93e127d8c325821
SedaKirakosyan <89702829+SedaKirakosyan@users.noreply.github.com>
2021-10-02 00:15:52 +0400

    Good work^_^

---
+++ home30.py
@@ -0,0 +1,38 @@
+"""4. sum word
+Given a list of strings. Find the strings with maximum and 
+minimum lengths in array. Print the sum of their lengths."""
+# list_ = ["anymore", "raven", "me", "communicate"]
+# leng = []
+# for i in range(len(list_)):
+# 	leng.append(len(list_[i]))
+# print(max(leng)+min(leng))
+
+"""5. find index
+Given a list of numbers and a number. Find the index of a 
+first element which is equal to that number. If there is not 
+such a number, that find the index of the first element 
+which is the closest to it."""
+# def find(my_list):
+# 	for i in range(len(my_list)):
+# 		if my_list[i] == number:
+# 			return i
+# 		else:
+# 			return min(range(len(my_list)), key = lambda i: abs(my_list[i]-number))
+# number = int(input("number: "))
+# my_list = [21, -9, 15, 2116, -71, 33]
+# print(find(my_list))
+
+"""8. FIBONACCI
+Write a function using recursion to find fibonacci numbers:"""
+# def fib(n):
+# 	if n == 1:
+# 		return 0
+# 	elif n == 2:
+# 		return 1
+# 	else:
+# 		return fib(n-1) +fib(n-2)
+# n = int (input(""))
+# print(fib(n))
+
+
+
